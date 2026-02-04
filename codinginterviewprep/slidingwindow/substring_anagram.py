f4bb4953add01b36bd78dc079ee9d0f0e30fa7ae
camcast3 <carltoncameron50@gmail.com>
2026-02-04 05:09:51 +0000

    Refactor substring anagram functions for improved performance and clarity

--- codinginterviewprep/slidingwindow/substring_anagram.py
+++ codinginterviewprep/slidingwindow/substring_anagram.py
@@ -1,4 +1,6 @@
-def substring_anagram(s, t) -> int:
+# Current Implementation is O(n^2)
+
+def substring_anagram_worst(s, t) -> int:
     window_size = len(t)
     string_length = len(s)
 
@@ -27,3 +29,39 @@ def is_anagram(sub, t) -> bool:
         freq[ord(c) - ord('a')] -= 1
 
     return all(v == 0 for v in freq)
+
+
+def substring_anagram(s, t) -> int:
+    """
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+    window_size = len(t)
+    string_length = len(s)
+    count = 0
+
+    if window_size > string_length:
+        return count
+
+    expected_freqs, window_freqs = [0] * 26, [0] * 26
+    s = s.lower()
+    t = t.lower()
+
+    for c in t:
+        expected_freqs[ord(c) - ord('a')] += 1
+
+    left = right = 0
+
+    while right < string_length:
+        window_freqs[ord(s[right]) - ord('a')] += 1
+
+        if right - left + 1 == window_size:
+            if expected_freqs == window_freqs:
+                count += 1
+
+            window_freqs[ord(s[left]) - ord('a')] -= 1
+            left += 1
+
+        right += 1
+
+    return count
